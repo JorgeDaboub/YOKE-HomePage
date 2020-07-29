94581b378e51fe9a7d81beaff408a97c324f2ac0
Jorge <jdaboub@nd.edu>
2020-07-29 11:37:31 -0600

    Initial Commit

---
+++ YOKE HomeScreen/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YOKE HomeScreen
+//
+//  Created by Jorge Daboub on 7/29/20.
+//  Copyright © 2020 Jorge Daboub. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
